72aa003ba07d3d03652195b0bf08d03288c1ba66
mfaziz <mfaziz50@gmail.com>
2024-05-30 21:48:54 +0700

    slicing student home screen

--- lib/presentation/screens/student/student_home_screen.dart
+++ lib/presentation/screens/student/student_home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:iconsax/iconsax.dart';
 import 'package:peqing/bloc/auth/auth_bloc.dart';
 import 'package:peqing/core/constant/constant.dart';
 import 'package:peqing/core/theme/app_colors.dart';
@@ -29,6 +30,8 @@ class _StudentHomeScreenState extends State<StudentHomeScreen> {
                 _buildQRCode(context),
                 const SizedBox(height: 16),
                 _buildHeading('Mata Kuliah Kamu'),
+                const SizedBox(height: 16),
+                _buildSubject(),
               ],
             ),
           ),
@@ -58,6 +61,107 @@ class _StudentHomeScreenState extends State<StudentHomeScreen> {
           ),
         );
       },
+    );
+  }
+
+  Widget _buildSubject() {
+    return GridView.count(
+      physics: const NeverScrollableScrollPhysics(),
+      crossAxisCount: 2,
+      shrinkWrap: true,
+      crossAxisSpacing: 15.0,
+      mainAxisSpacing: 15.0,
+      childAspectRatio: 1.3,
+      padding: const EdgeInsets.only(bottom: 24.0),
+      children: [
+        _buildCivitasCard(),
+        _buildCivitasCard(),
+      ],
+    );
+  }
+
+  Widget _buildCivitasCard() {
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        padding: const EdgeInsets.all(16.0),
+        decoration: BoxDecoration(
+            color: AppColors.white,
+            borderRadius: BorderRadius.circular(24),
+            border: Border.all(color: AppColors.dark[100]!)),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            MenuAnchor(
+              style: MenuStyle(
+                elevation: WidgetStateProperty.all(0.0),
+                backgroundColor: WidgetStateProperty.all(Colors.transparent),
+                shape: WidgetStatePropertyAll(RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16))),
+              ),
+              menuChildren: <Widget>[
+                Container(
+                    width: 70,
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 16.0, vertical: 8.0),
+                    decoration: BoxDecoration(
+                        color: AppColors.danger[500],
+                        borderRadius: BorderRadius.circular(99)),
+                    child: Center(
+                        child: Text('Hapus',
+                            style: Theme.of(context)
+                                .textTheme
+                                .titleSmall!
+                                .copyWith(color: AppColors.white)))),
+                Container(
+                    width: 70,
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 16.0, vertical: 8.0),
+                    decoration: BoxDecoration(
+                        color: AppColors.secondary[500],
+                        borderRadius: BorderRadius.circular(99)),
+                    child: Center(
+                        child: Text('Edit',
+                            style: Theme.of(context)
+                                .textTheme
+                                .titleSmall!
+                                .copyWith(color: AppColors.white)))),
+              ],
+              builder: (BuildContext context, MenuController controller,
+                      Widget? child) =>
+                  GestureDetector(
+                onTap: () {
+                  if (controller.isOpen) {
+                    controller.close();
+                  } else {
+                    controller.open();
+                  }
+                },
+                child: Container(
+                  padding: const EdgeInsets.all(9.33),
+                  decoration: BoxDecoration(
+                      color: AppColors.white,
+                      borderRadius: BorderRadius.circular(99),
+                      border: Border.all(color: AppColors.dark[100]!)),
+                  child: Icon(
+                    Iconsax.more_circle5,
+                    color: AppColors.dark[500]!,
+                    size: 16,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 8.0),
+            Text('Workshop Flutter',
+                overflow: TextOverflow.ellipsis,
+                style: Theme.of(context).textTheme.titleMedium),
+            const SizedBox(height: 4),
+            Text('Fulan bin Fulan',
+                overflow: TextOverflow.ellipsis,
+                style: Theme.of(context).textTheme.bodyMedium),
+          ],
+        ),
+      ),
     );
   }
 
